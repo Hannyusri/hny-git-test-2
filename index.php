70b33b76400de16d9b7cb5a13eea1e19607ba718
Farhan Yusri <farhan.220170125@mhs.unimal.ac.id>
2024-10-31 17:23:50 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente rerum vero recusandae velit, quidem quia ullam pariatur numquam! Voluptatem tempora neque autem, at sint necessitatibus laudantium maxime eligendi eos ratione?</p>
-    
+<li>satu</li>
+<li>dua</li>
 </body>
 </html>
